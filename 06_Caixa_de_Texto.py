b4d2851f2659568fce23d08620d08ffdbca06b3c
Elton John Robaina da Silva <eltonj.robaina@gmail.com>
2022-01-17 12:02:08 -0300

    Caixa de texto
    Caixa de texto

--- 06_Caixa_de_Texto.py
+++ 06_Caixa_de_Texto.py
@@ -1,6 +1,6 @@
 from cProfile import label
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QLabel, QLineEdit
 from PyQt5 import QtGui
 
 class Janela (QMainWindow):
@@ -14,6 +14,11 @@ class Janela (QMainWindow):
         self.altura = 600
         self.titulo = "Primeira Janela"
         #
+        ## Caixa de texto
+        self.caixa_texto = QLineEdit(self)                        #caixa de texto
+        self.caixa_texto.move(20,10)
+        self.caixa_texto.resize(250,30)
+
 
         ### OBJETO
         ## Botao e uma instancia da classe QPushButton
@@ -23,17 +28,32 @@ class Janela (QMainWindow):
         botao1.setStyleSheet('QPushButton {background-color: #696969; font:bold; font-size:20px}')
         botao1.clicked.connect(self.botao1_click)
 
+        ## Botao Caixa
+        botao_caixa = QPushButton('Enviar Texto', self)
+        botao_caixa.move(550,200) # distancia em pixel esquerda, distacia a altura
+        botao_caixa.resize(150,80) # largura, altura
+        botao_caixa.setStyleSheet('QPushButton {background-color: #008000; font:bold; font-size:20px}')
+        botao_caixa.clicked.connect(self.mostra_texto)
+
         ## Botao e uma instancia da classe QPushButton
         botao2 = QPushButton('Botao 2', self)
         botao2.move(350,200) # distancia em pixel esquerda, distacia a altura
         botao2.resize(150,80) # largura, altura
         botao2.setStyleSheet('QPushButton {background-color: #0000FF ; font:bold; font-size:20px}')
         botao2.clicked.connect(self.botao2_click)
+
+        #labels
         self.label_1 = QLabel(self)
-        self.label_1.setText("Aperte um Botao!!!")
         self.label_1.move(50,50)
         self.label_1.setStyleSheet('QLabel {font:bold; font-size:25px; color:"red"}')
         self.label_1.resize(400,25)
+        self.label_1.setText("Aperte um Botao!!!")
+
+        self.label_caixa = QLabel(self)
+        self.label_caixa.move(375,50)
+        self.label_caixa.setStyleSheet('QLabel {font:bold; font-size:25px; color:"black"}')
+        self.label_caixa.resize(400,30)
+        self.label_caixa.setText("Digitou: ")
 
         ## IMGEM Label
         self.carro = QLabel(self)
@@ -56,6 +76,10 @@ class Janela (QMainWindow):
         self.label_1.setStyleSheet('QLabel {font:bold; font-size:25px; color:"black"}')
         self.carro.setPixmap(QtGui.QPixmap('carro1.png'))
     
+    def mostra_texto(self):
+        conteudo = self.caixa_texto.text()
+        self.label_caixa.setText("Digitou: " + conteudo)
+
     def botao2_click(self):
         #print('O Botao 2 foi clicado!!!!')
         self.label_1.setText("O carro 2 foi selecionado")
